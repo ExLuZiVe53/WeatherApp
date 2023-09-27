3f7349791227b29e17208eae0c6e2c91d369af87
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-27 15:15:31 +0200

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export async function fetchWeather(city, setError) {
-  const API_KEY = c46f07553e96b04e968aa741d19331d5;
-  const url = `api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}`;
+  // const API_KEY = c46f07553e96b04e968aa741d19331d5;
+  const url = `api.openweathermap.org/data/2.5/forecast?id=524901&appid={c46f07553e96b04e968aa741d19331d5}`;
 
   try {
     const response = await axios.get(url);
