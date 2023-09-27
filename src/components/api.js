0f305860f3cb00c5606a92aefa1c21e2ec9b4b76
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-27 14:47:26 +0200

    added to icons

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export async function fetchWeather(city, setError) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
+  const API_KEY = c46f07553e96b04e968aa741d19331d5;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
   try {
     const response = await axios.get(url);
